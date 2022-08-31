f1d2c6f713857b0d1bfe3ec9edf91062837ebc73
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-08-31 12:27:05 +0200

    Create css file, add class to div

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ const baseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 //LOOP FOR OTHER IMG POKEMON
-for (let i = 1; i < 151; i++) {
+for (let i = 1; i <= 151; i++) {
   const pokemon = document.createElement("div");
+  pokemon.classList.add("pokemon");
   const label = document.createElement("span");
   label.innerText = `#${i}`;
   const newImg = document.createElement("img");

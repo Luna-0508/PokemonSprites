7830bc8f323140af02db2425b63b6ea0b93fed48
Natalia Łukaszka <lukaszkanatalia94@gmail.com>
2022-08-31 12:20:25 +0200

    Create divs and numbers for imgs

--- app.js
+++ app.js
@@ -5,8 +5,14 @@ const baseURL =
   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
 //LOOP FOR OTHER IMG POKEMON
-for (let i = 1; i < 10; i++) {
+for (let i = 1; i < 151; i++) {
+  const pokemon = document.createElement("div");
+  const label = document.createElement("span");
+  label.innerText = `#${i}`;
   const newImg = document.createElement("img");
   newImg.src = `${baseURL}${i}.png`;
-  container.appendChild(newImg);
+
+  pokemon.appendChild(newImg);
+  pokemon.appendChild(label);
+  container.appendChild(pokemon);
 }
